55e069094f586edf029bb0ed46476217a2b89e50
Руслан Сайфуллоевич Каримов <r.karimov@intellectika.ru>
2020-02-13 10:37:55 +0300

    Нет ошибок

---
+++ TaskManager/Assets/Scripts/MongoDbAtlasManager.cs
@@ -0,0 +1,58 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace DataBase
+{
+    class MongoDbAtlasManager
+    {
+        public MongoClient _client;
+        public IMongoDatabase _database;
+        public IMongoCollection<BsonDocument> UserCollection;
+
+        private User _user;
+
+        private void Conect()
+        {
+            _client = new MongoClient("mongodb+srv://Rkarimov:12345@clustertask-nfnnn.gcp.mongodb.net/test?retryWrites=true&w=majority");
+            _database = _client.GetDatabase("test");
+            UserCollection = _database.GetCollection<BsonDocument>("Users");
+        }
+
+        public void TakeUser(string login, string password)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("login", login);
+
+            var response = UserCollection.Find(filter).FirstOrDefault();
+
+            var a = JsonConvert.DeserializeObject(response.ToString());
+        }
+
+        public string NewUser(string login, string password)
+        {
+            var filter = Builders<BsonDocument>.Filter.Eq("login", login);
+
+            var checkUser = UserCollection.Find(filter).FirstOrDefault();
+
+            if(checkUser != null)
+            {
+                return "User already created";
+            }
+
+            _user = new User
+            {
+                login = login,
+                password = password
+            };
+
+            var document = new BsonDocument { { "login", login }, {"password", password}};
+
+            UserCollection.InsertOne(document);
+
+            checkUser = UserCollection.Find(filter).FirstOrDefault();
+
+            return checkUser.ToString();
+        }
+    }
+}
---
+++ TaskManager/Assets/Scripts/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class User
+    {
+        public string id;
+        public string login;
+        public string password;
+
+    }
+}
